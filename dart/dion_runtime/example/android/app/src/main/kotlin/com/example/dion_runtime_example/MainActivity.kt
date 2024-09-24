f3afcd8f3f7ce632326a440f2587b893b7bb3c0d
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2024-09-24 08:12:03 +0200

    Revert "Start of dart/flutter binding"
    This reverts commit 638fa572982b942b7724cc432291efa4a5cf1243.

--- dart/dion_runtime/example/android/app/src/main/kotlin/com/example/dion_runtime_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dion_runtime_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- dart/dion_runtime/lib/dion_runtime.dart
+++
@@ -1,4 +0,0 @@
-library dion_runtime;
-
-export 'src/rust/api/simple.dart';
-export 'src/rust/frb_generated.dart' show RustLib;
